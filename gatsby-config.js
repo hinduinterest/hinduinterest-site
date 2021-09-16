0f1f3e24d1020fa2b416898c41d1be9ba0fa5e6e
Akhil Kodali <50161+akhil@users.noreply.github.com>
2021-09-16 10:05:55 -0700

    removed gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,18 +91,5 @@ module.exports = {
                 modules: ['gatsby-starter-morning-dew'],
             },
         },
-        /*
-        {
-            resolve: `gatsby-plugin-google-gtag`,
-            options: {
-                // You can add multiple tracking ids and a pageview event will be fired for all of them.
-                trackingIds: [
-                    "G-6ERREZJQVH", // Google Analytics / GA
-                    //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
-                    //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-                ],
-            },
-        },
-        */
     ],
 }
